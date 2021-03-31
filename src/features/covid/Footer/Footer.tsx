d0bea50fa25f9020e57a7e03fff0589569533d12
YusukeYoshihiro <yusuke.10.25.61@gmail,com>
2021-03-30 23:40:56 -0700

    modified

--- src/features/covid/Footer/Footer.tsx
+++ src/features/covid/Footer/Footer.tsx
@@ -5,7 +5,7 @@ import styles from './Footer.module.css';
 const Footer:React.FC = () => {
   return (
     <footer className={styles.footerStyles}>
-      Copyright &copy; 2021 Yusuke Yoshihiro <br/>Referring <a href="https://covid19api.com"> &nbsp; A free API for data on the Coronavirus </a>
+      Copyright &copy;  {new Date().getFullYear()}  Yusuke Yoshihiro <br/>Referring <a href="https://covid19api.com"> &nbsp; A free API for data on the Coronavirus </a>
     </footer>
   )
 }
